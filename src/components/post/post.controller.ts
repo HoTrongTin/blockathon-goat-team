9259976fa24a52baa12fd1d3574d03677c972256
TinHoC98 <tinht@coin98.finance>
2024-05-04 18:39:35 +0700

    crud post

--- src/components/post/post.controller.ts
+++ src/components/post/post.controller.ts
@@ -1,7 +1,7 @@
-import { Body, Controller, Post, UseGuards, Request, Get, Query } from '@nestjs/common'
-import { PostService } from './post.service'
+import { Body, Controller, Delete, Get, Param, Patch, Post, Request, UseGuards } from '@nestjs/common'
 import { CustomAuthGuard } from '../auth/guard/custom-auth.guard'
 import { CreatePostDto } from './dto/create-post.dto'
+import { PostService } from './post.service'
 
 @Controller('post')
 export class PostController {
@@ -14,8 +14,22 @@ export class PostController {
     return this.postService.createPost(walletAddress, createPostDto)
   }
 
+  @UseGuards(CustomAuthGuard)
+  @Patch('/:id')
+  async updatePost(@Param('id') id: number, @Request() req: Request, @Body() createPostDto: CreatePostDto) {
+    const walletAddress = req['user'].walletAddress
+    return this.postService.updatePost(walletAddress, id, createPostDto)
+  }
+
+  @UseGuards(CustomAuthGuard)
+  @Delete('/:id')
+  async deletePost(@Param('id') id: number, @Request() req: Request) {
+    const walletAddress = req['user'].walletAddress
+    return this.postService.deletePost(walletAddress, id)
+  }
+
   @Get('/:id')
-  async getPostById(@Query('id') id: number) {
+  async getPostById(@Param('id') id: number) {
     return this.postService.getPostById(id)
   }
 }
--- src/components/post/post.service.ts
+++ src/components/post/post.service.ts
@@ -67,13 +67,73 @@ export class PostService {
     return this.getPostById(newPost.id)
   }
 
+  async updatePost(walletAddress: any, id: number, data: CreatePostDto) {
+    const post = await this.postRepository.findOne({ id })
+    if (!post || post.ownerAddress !== walletAddress) {
+      throw new Error('Post not found')
+    }
+
+    post.name = data.name
+    post.description = data.description
+    post.isPublic = data.isPublic
+    await this.postRepository.save(post)
+
+    await this.postAssetRepository.delete({ postId: id })
+
+    if (data.images) {
+      for (const image of data.images) {
+        const postAsset = new PostAssetEntity()
+        postAsset.postId = id
+        postAsset.assetType = AssetType.image
+        postAsset.assetValue = image
+        await this.postAssetRepository.save(postAsset)
+      }
+    }
+
+    if (data.videos) {
+      for (const video of data.videos) {
+        const postAsset = new PostAssetEntity()
+        postAsset.postId = id
+        postAsset.assetType = AssetType.video
+        postAsset.assetValue = video
+        await this.postAssetRepository.save(postAsset)
+      }
+    }
+
+    if (data.whitelistAddresses) {
+      for (const address of data.whitelistAddresses) {
+        const postAsset = new PostAssetEntity()
+        postAsset.postId = id
+        postAsset.assetType = AssetType.whitelistAddress
+        postAsset.assetValue = address
+        await this.postAssetRepository.save(postAsset)
+      }
+    }
+
+    return this.getPostById(id)
+  }
+
+  async deletePost(walletAddress: any, id: number) {
+    const post = await this.postRepository.findOne({ id })
+    if (!post || post.ownerAddress !== walletAddress) {
+      throw new Error('Post not found')
+    }
+
+    await this.postRepository.delete({ id })
+    await this.postAssetRepository.delete({ postId: id })
+
+    return { message: 'Post deleted successfully' }
+  }
+
   async getPostById(postId: number): Promise<{
     post: PostEntity
     images: string[]
     videos: string[]
     whitelistAddresses: string[]
   }> {
+    console.log('postId: ', postId)
     const post = await this.postRepository.findOne({ id: postId })
+    console.log('post: ', post)
 
     const postAssets = await this.postAssetRepository.find({ postId })
     const images = postAssets.filter(asset => asset.assetType === AssetType.image).map(asset => asset.assetValue)
