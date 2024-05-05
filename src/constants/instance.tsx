f56090bb22cc3b5b778c5099c0b33360f51940bd
An To <thanhanto0604@gmail.com>
2024-05-05 08:49:41 +0700

    save Contract

--- src/constants/instance.tsx
+++ src/constants/instance.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const GOAT_CONTRACT = '0xA2b34213723B019Aec1695A7c2a21539AAeABA54'
-export const NFT_Contract ='0xB7E664E9CE178Bd8aF9BeCdb880C9d762C249a53'// '0x580ba32A3Fc52DceaC12721858333E8376A39cA2'
+export const NFT_Contract ='0xB7E664E9CE178Bd8aF9BeCdb880C9d762C249a53'
 export const MeOnChainServiceInstance = axios.create({
   baseURL: import.meta.env.VITE_BE_LINK
 })
